39186a4a991fdf0022e8ac3462e199383312fbf8
Stas Dulman <stasdes@gmail.com>
2020-06-10 16:21:31 +0300

    add validation

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters')
+  ],
+  (req: Request, res: Response) => {
+    console.log('sign up');
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    if (!email || typeof email !== 'string') {
+      res.status(400).send('Provide a valid email');
+    }
+    res.send({});
+    // new User({ email, password })
+  }
+);
+
+export { router as signupRouter };
