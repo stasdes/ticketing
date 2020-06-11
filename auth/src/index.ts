6b910e686759c8e4799b69d2328dc222dab43708
Stas Dulman <stasdes@gmail.com>
2020-06-11 14:52:31 +0300

    add cookie and jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -36,8 +44,8 @@ const start = async () => {
     console.error(err);
   }
 
-  app.listen(3500, () => {
-    console.log('Listening on port 3500!!!!!!!!');
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
   });
 };
 
