35f4aade6307a4f5fa8d3cdddeedb1ef933134d8
Stas Dulman <stasdes@gmail.com>
2020-06-15 15:30:56 +0300

    move to common

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,24 +1,21 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@stasd/common';
 
 import { Password } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
   '/api/users/signin',
   [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
+    body('email').isEmail().withMessage('Email must be valid'),
     body('password')
       .trim()
       .notEmpty()
-      .withMessage('You must supply a password')
+      .withMessage('You must supply a password'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
