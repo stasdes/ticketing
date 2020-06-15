34791f8248f12cd55af97bc9160864ff4b2ad35d
Stas Dulman <stasdes@gmail.com>
2020-06-15 14:36:56 +0300

    fix the url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,9 +3,12 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
-
+    // to get it:
+    // http://<serice name>.<namespace>.svc.cluster.local
+    // kubectl get namespace - to get namespace name
+    // kubectl get services -n ingress-nginx - to get service name
     return axios.create({
-      baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
     });
   } else {
