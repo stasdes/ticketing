ecc2fd193c77692a501cfb35adf3cc1ce34590ae
Stas Dulman <stasdes@gmail.com>
2020-06-16 10:42:10 +0300

    add tickets ms impl

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,15 +3,18 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  if (!process.env.JWT_KEY || !process.env.MONGO_URI) {
-    throw new Error('JWT_KEY and MONGO_URI must be defined');
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
   }
 
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     });
     console.log('Connected to MongoDb');
   } catch (err) {
