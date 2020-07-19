0dda2745cdbb4a5dc103df4cbb7e16d8e8a79107
Stas Dulman <stasdes@gmail.com>
2020-07-19 15:19:21 +0300

    add .

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Start auth service')
+  console.log('Start auth service.')
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
